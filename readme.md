eae99356873777a5ff968fb9e6d53951c522ae37
RGBboy <me@rgbboy.com>
2012-10-04 17:55:28 +1000

    initial commit

---
+++ readme.md
@@ -0,0 +1,64 @@
+# Basic User Model
+
+  A barebones User Model to extend from.
+
+  [![Build Status](https://secure.travis-ci.org/RGBboy/basic-user-model.png)](http://travis-ci.org/RGBboy/basic-user-model)
+
+## Features
+
+  * Simple, Simple, Simple! Just enough to get you started.
+  * Bcrypted passwords.
+  * Validation.
+  * Use as is, or extend it to add your own properties.
+
+## Installation
+
+    npm install git://github.com/RGBboy/basic-user-model.git
+
+## Usage
+
+Require it:
+
+``` javascript
+  User = require('basic-user-model');
+```
+
+## Requires
+
+  * Mongoose.js
+  * bcrypt
+  * validator (node-validator)
+
+## Todo
+
+  
+  * role? emailToken? and emailTokenCreated? Move to an extended module?
+  * Figure out how to make an extensible Model.
+  * Write rest of Model Tests:
+    - write tests for id property
+    - write tests for instance methods
+
+## License 
+
+(The MIT License)
+
+Copyright (c) 2012 RGBboy &lt;me@rgbboy.com&gt;
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+'Software'), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ test/basic-user-model.test.js
@@ -0,0 +1,229 @@
+// User Unit Test
+//
+
+// MODULE DEPENDENCIES
+// -------------------
+
+var User = require('../index'),
+    mongoose = require('mongoose'),
+    should = require('should'),
+    fakeUser;
+
+// TESTS
+// -----
+
+describe.only('Basic User Model', function () {
+
+  before(function (done) {
+    if (!mongoose.connection.db) {
+      mongoose.connect('mongodb://basic-user-model-test:basic-user-model-test@localhost/basic-user-model-test');
+    };
+    done();
+  });
+
+  beforeEach(function (done) {
+    fakeUser = {
+      email: 'TestUser@test.com',
+      password: 'TestPassword',
+      passwordConfirm: 'TestPassword'
+    };
+    User.remove(done);
+  });
+
+  describe('Static Methods', function () {
+
+    describe('.register', function () {
+
+      describe('when correct credentials are used', function () {
+
+        it('should create a new user with user object', function (done) {
+          User.register(fakeUser, function (err, user) {
+            should.not.exist(err);
+            should.exist(user);
+            User.find({}, function(err, users) {
+              users.length.should.equal(1);
+              done();
+            });
+          })
+        });
+
+        it('should callback with an error if user already exists', function (done) {
+          User.register(fakeUser, function (err, user) {
+            should.not.exist(err);
+            should.exist(user);
+            User.register(fakeUser, function (err, user) {
+              should.exist(err);
+              done();
+            });
+          });
+        });
+
+        it('should create a cryptedPassword if password and passwordConfirm are correct', function (done) {
+          User.register(fakeUser, function(err, user) {
+            should.exist(user.cryptedPassword);
+            done();
+          });
+        });
+
+      });
+
+      describe('when incorrect credentials are used', function () {
+
+        it('should callback with an error if email is missing', function (done) {
+          delete fakeUser.email;
+          User.register(fakeUser, function(err, user) {
+            should.exist(err);
+            done();
+          });
+        });
+
+        it('should callback with an error if if email is empty', function (done) {
+          fakeUser.email = '';
+          User.register(fakeUser, function(err, user) {
+            should.exist(err);
+            done();
+          });
+        });
+
+        it('should callback with an error if if email is not a valid email', function (done) {
+          fakeUser.email = 'testtest.com';
+          User.register(fakeUser, function(err, user) {
+            should.exist(err);
+            done();
+          });
+        });
+
+        it('should callback with an error if the password and passwordConfirm do not match', function (done) {
+          fakeUser.passwordConfirm = 'NotTestPassword';
+          User.register(fakeUser, function(err, user) {
+            should.exist(err);
+            done();
+          });
+        });
+
+        it('should callback with an error if the password is missing', function (done) {
+          delete fakeUser.password;
+          User.register(fakeUser, function(err, user) {
+            should.exist(err);
+            done();
+          });
+        });
+
+        it('should callback with an error if the password is empty', function (done) {
+          fakeUser.password = '';
+          User.register(fakeUser, function(err, user) {
+            should.exist(err);
+            done();
+          });
+        });
+
+        it('should callback with an error if the passwordConfirm is missing', function (done) {
+          fakeUser.passwordConfirm = '';
+          User.register(fakeUser, function(err, user) {
+            should.exist(err);
+            done();
+          });
+        });
+
+        it('should callback with an error if the passwordConfirm is empty', function (done) {
+          delete fakeUser.passwordConfirm;
+          User.register(fakeUser, function(err, user) {
+            should.exist(err);
+            done();
+          });
+        });
+
+      });
+
+    });
+
+    describe('.findByEmailToken', function () {
+
+      beforeEach(function (done) {
+        User.register(fakeUser, function (err, user) {
+          fakeUser = user;
+          done();
+        });
+      });
+
+      describe('when token is valid', function () {
+
+        it('should callback with a user', function (done) {
+          User.findByEmailToken(fakeUser.emailToken, function (err, user) {
+            should.not.exist(err);
+            should.exist(user);
+            done();
+          });
+        });
+
+      });
+
+      describe('when token is invalid', function () {
+
+        it('should callback with nothing if token does not exist', function (done) {
+          User.remove(function () {
+            User.findByEmailToken(fakeUser.emailToken, function (err, user) {
+              should.not.exist(err);
+              should.not.exist(user);
+              done();
+            });
+          });
+        });
+
+        it('should callback with an error if token is older than 2 hours', function (done) {
+          // change tokenCreated to 2 hours ago
+          var time = Date.now() - (2 * 60 * 60 * 1000);
+          User.findOne({ email: fakeUser.email }, function (err, user) {
+            user.emailTokenCreated = time;
+            user.save(function (err, user) {
+              User.findByEmailToken(user.emailToken, function (err, user) {
+                should.exist(err);
+                should.not.exist(user);
+                done();
+              })
+            });
+          });
+        });
+
+      });
+
+    });
+
+    describe('.findByEmail', function () {
+
+      describe('when user exists', function () {
+
+        beforeEach(function (done) {
+          User.register(fakeUser, function (err, user) {
+            fakeUser = user;
+            done();
+          });
+        });
+
+        it('should callback with a user', function (done) {
+          User.findByEmail(fakeUser.email, function (err, user) {
+            should.not.exist(err);
+            should.exist(user);
+            done();
+          });
+        });
+
+      });
+
+      describe('when user does not exist', function () {
+
+        it('should callback with nothing if user does not exist', function (done) {
+          User.findByEmail(fakeUser.email, function (err, user) {
+            should.not.exist(err);
+            should.not.exist(user);
+            done();
+          });
+        });
+
+      });
+
+    });
+
+  });
+
+});
